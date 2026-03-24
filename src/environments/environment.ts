05c7b0c052877a2477dc5f0624d9ff430ef86e58
DavideTeta <d.teta@neotech.srl>
2026-03-24 14:24:23 +0100

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 export const environment = {
     production: false,
     // Come web-gym-iscritto: in dev chiamate dirette a ms-gym-backoffice (porta 8080)
-    apiUrl: 'https://www.gyminvestire.neotech.srl/api/backoffice-ms',
+    apiUrl: 'https://gyminvestire.neotech.srl/api/backoffice-ms',
   };
